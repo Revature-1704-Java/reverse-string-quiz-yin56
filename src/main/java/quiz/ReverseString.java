760ffd93751ae5cc4b8f1c07f52c87cfbd038c62
Samuel Yin <yin56@purdue.edu>
2017-12-20 07:09:36 -0800

    added comments

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -5,9 +5,12 @@ public class ReverseString {
         StringBuffer rev = new StringBuffer();
         int strlen = input.length();
         
+        //if string is empty return it
         if(input.equals("")){
             return input;
         }
+
+        //reverse the string
         for(int i = strlen; i > 0; i--){
             rev = rev.append(input.charAt(i-1));
         }
